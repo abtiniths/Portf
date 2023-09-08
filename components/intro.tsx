1e46d6ddc492950565e9cd61f45528d72c5a0190
abtiniths <abtin.nouran@iths.se>
2023-09-08 15:56:38 +0200

    initial push, fixed bg color style setup and started on main panel

---
+++ components/intro.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Intro() {
+  return;
+
+  <section className="">
+    <div className=" flex items-center justify-center">
+      <div>
+        <img src="@/public/IMG-rätt" alt="" />
+      </div>
+    </div>
+  </section>;
+}
